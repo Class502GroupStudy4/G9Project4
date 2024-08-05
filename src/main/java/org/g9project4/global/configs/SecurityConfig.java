190ccf1ec58f652640c5c175f55527d2529d29d7
inseokLee999 <syt06137@naver.com>
2024-08-05 14:18:01 +0900

    configs 들 추가

--- src/main/java/org/g9project4/global/configs/SecurityConfig.java
+++ src/main/java/org/g9project4/global/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.g9project4.global.configs;
 
+
 import org.g9project4.member.services.LoginFailureHandler;
 import org.g9project4.member.services.LoginSuccessHandler;
 import org.g9project4.member.services.MemberAuthenticationEntryPoint;
@@ -21,31 +22,36 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         /* 로그인, 로그아웃 S */
-        http.formLogin(f->{
+        http.formLogin(f -> {
             f.loginPage("/member/login")
-                    .usernameParameter("username")
+                    .usernameParameter("email")
                     .passwordParameter("password")
                     .successHandler(new LoginSuccessHandler())
                     .failureHandler(new LoginFailureHandler());
+//                    .successForwardUrl("/")
+//                    .failureUrl("/member/login?error=true");
         });
+
         http.logout(logout -> {
             logout.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+//                    .logoutSuccessHandler()
                     .logoutSuccessUrl("/member/login");
         });
         /* 로그인, 로그아웃 E */
         /* 인가(접근 통제) 설정 S*/
         http.authorizeRequests(authorizeRequests -> {
             authorizeRequests.requestMatchers("/mypage/**").authenticated()//회원 전용
-                    .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
+                    .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")//예외 발생
                     .anyRequest().permitAll();
         });
         http.exceptionHandling(c -> {
-            c.authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+            c.authenticationEntryPoint(new MemberAuthenticationEntryPoint())//예외 가
                     .accessDeniedHandler((req, res, e) -> {
                         res.sendError(HttpStatus.UNAUTHORIZED.value());
                     });
         });
         /* 인가(접근 통제) 설정 E*/
+        //iframe 자원 출처를 같은 서버 자원으로 한정
         http.headers(c -> c.frameOptions(f -> f.sameOrigin()));
         return http.build();
     }
