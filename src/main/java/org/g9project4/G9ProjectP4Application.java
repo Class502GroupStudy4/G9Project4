ec4ec6ecd266bbe3d4e98933ea7eec088e0f393b
yoojung275 <yoojung275@gmail.com>
2024-08-07 13:43:22 +0900

    @EnableDiscoveryClient 추가 (강사님이 꼭 넣으라고 하심)

--- src/main/java/org/g9project4/G9ProjectP4Application.java
+++ src/main/java/org/g9project4/G9ProjectP4Application.java
@@ -1,9 +1,12 @@
 package org.g9project4;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class G9ProjectP4Application {
 
 	public static void main(String[] args) {
