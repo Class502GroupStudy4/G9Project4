b9f97545482712dde678aa7d22c3345c158b273b
irine3355 <irine3355@gmail.com>
2024-08-07 10:41:40 +0900

    메인페이지 1차

--- src/main/java/org/g9project4/main/controllers/MainController.java
+++ src/main/java/org/g9project4/main/controllers/MainController.java
@@ -1,14 +1,21 @@
 package org.g9project4.main.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/")
 @Controller
 public class MainController {
+
     @GetMapping
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("addCss", "main");//메인페이지
+        model.addAttribute("addScript", "main"); //메인페이지
+
         return "front/main/index";
     }
 }
--- src/main/resources/static/front/js/main.js
+++ src/main/resources/static/front/js/main.js
@@ -1,21 +1,9 @@
 /*메인페이지 상단 바탕이미지 시작 */
+
 document.addEventListener("DOMContentLoaded", function() {
-    const images = [
-        'front/images/background1.jpg',
-        'front/images/background2.jpg'
-    ];
-    let currentIndex = 0;
-
-    function changeBackgroundImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
-    }
-
-    // 처음 배경 이미지 설정
-    document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
-
-    // 5초마다 배경 이미지 변경
-    setInterval(changeBackgroundImage, 5000);
+    const mainEl = document.querySelector("main");
+    setInterval(function() {
+        mainEl.classList.toggle("change");
+    }, 5000);
 });
-
 /*메인페이지 상단 바탕이미지 끝 */
