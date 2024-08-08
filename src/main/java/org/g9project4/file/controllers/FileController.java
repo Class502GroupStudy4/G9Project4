8983bbd234bbe3d9c482ffcf2d46411a7bf7ab4e
yoojung275 <yoojung275@gmail.com>
2024-08-08 11:03:39 +0900

    마이페이지 - 프로필 사진 업로드 기능 구현 중

--- src/main/java/org/g9project4/file/controllers/FileController.java
+++ src/main/java/org/g9project4/file/controllers/FileController.java
@@ -1,5 +1,6 @@
 package org.g9project4.file.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.g9project4.file.entities.FileInfo;
 import org.g9project4.file.services.FileUploadService;
@@ -7,6 +8,7 @@ import org.g9project4.global.exceptions.RestExceptionProcessor;
 import org.g9project4.global.rests.JSONData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,9 +23,13 @@ public class FileController implements RestExceptionProcessor {
 
     @PostMapping("/upload")//파일은 post 형태로 넘어온다
     public ResponseEntity<JSONData> upload(@RequestPart("file") MultipartFile[] files,//어떤 필드 인지 알려주는 역할(name값)
-                                           @RequestParam(name = "gid", required = false) String gid,//form 안에 있는 파라미터
-                                           @RequestParam(name = "location", required = false) String location) {
-        List<FileInfo> items = uploadService.upload(files, gid, location);
+                                           @Valid RequestUpload form, Errors errors) {
+
+        if (errors.hasErrors()) {
+
+        }
+
+        List<FileInfo> items = uploadService.upload(files, form.getGid(), form.getLocation());
         HttpStatus status = HttpStatus.CREATED;
         JSONData data = new JSONData(items);
         data.setStatus(status);
---
+++ src/main/resources/static/front/js/member/join.js
@@ -0,0 +1,8 @@
+/**
+ * 프로필 이미지 후속 처리
+ * 
+ * @param files
+ */
+function fileUploadCallback(files) {
+    console.log(files);
+}
