47bb0eb4827c57c0e8563737a0668e72a50da3b5
yoojung275 <yoojung275@gmail.com>
2024-08-16 11:50:06 +0900

    게시판 구현 중

--- src/main/java/org/g9project4/board/services/BoardInfoService.java
+++ src/main/java/org/g9project4/board/services/BoardInfoService.java
@@ -9,18 +9,22 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.g9project4.board.controllers.BoardDataSearch;
+import org.g9project4.board.controllers.RequestBoard;
 import org.g9project4.board.entities.BoardData;
 import org.g9project4.board.entities.QBoardData;
 import org.g9project4.board.exceptions.BoardDataNotFoundException;
 import org.g9project4.board.repositories.BoardDataRepository;
 import org.g9project4.global.ListData;
 import org.g9project4.global.Pagination;
+import org.g9project4.global.constants.DeleteStatus;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -36,11 +40,14 @@ public class BoardInfoService {
      *
      * @return
      */
-    public ListData<BoardData> getList(BoardDataSearch search) {
+    public ListData<BoardData> getList(BoardDataSearch search, DeleteStatus status) {
 
         int page = Math.max(search.getPage(), 1);
         int limit = search.getLimit();
         int offset = (page - 1) * limit;
+
+        //삭제가 되지 않은 게시글 목록이 기본값
+        status = Objects.requireNonNullElse(status, DeleteStatus.UNDELETED);
 
         String sopt = search.getSopt();
         String skey = search.getSkey();
@@ -51,12 +58,21 @@ public class BoardInfoService {
         /* 검색 처리 S */
         QBoardData boardData = QBoardData.boardData;
         BooleanBuilder andBuilder = new BooleanBuilder();
+        
+        //삭제, 미삭제 게시글 조회 처리
+        if (status != DeleteStatus.ALL) {
+            if (status == DeleteStatus.UNDELETED) {
+                andBuilder.and(boardData.deletedAt.isNull()); //미삭제된 게시글
+            } else {
+                andBuilder.and(boardData.deletedAt.isNotNull()); //삭제된 게시글
+            }
+        }
 
         if (bid != null && StringUtils.hasText(bid.trim())) { // 게시판별 조회
             bids = List.of(bid);
         }
 
-        if (bids != null && !bids.isEmpty()) { // 게시판 여러개 조회
+        if (bids != null && !bids.isEmpty()){ // 게시판 여러개 조회
             andBuilder.and(boardData.board.bid.in(bids));
         }
 
@@ -130,7 +146,7 @@ public class BoardInfoService {
         orderSpecifiers.add(boardData.createdAt.desc());
         /* 정렬 처리 E */
 
-        /* 목록 조회 처리 S*/
+        /* 목록 조회 처리 S */
         List<BoardData> items = queryFactory
                 .selectFrom(boardData)
                 .leftJoin(boardData.board)
@@ -143,15 +159,15 @@ public class BoardInfoService {
                 .limit(limit)
                 .fetch();
 
-        //추가 정보 처리
+        // 추가 정보 처리
         items.forEach(this::addInfo);
 
-        /* 목록 조회 처리 E*/
+        /* 목록 조회 처리 E */
 
         // 전체 게시글 갯수
         long total = repository.count(andBuilder);
 
-        //페이징 처리
+        // 페이징 처리
         Pagination pagination = new Pagination(page, (int)total, 10, limit, request);
 
         return new ListData<>(items, pagination);
@@ -164,38 +180,90 @@ public class BoardInfoService {
      * @param search
      * @return
      */
-    public ListData<BoardData> getList(String bid, BoardDataSearch search) {
+    public ListData<BoardData> getList(String bid, BoardDataSearch search, DeleteStatus status) {
         search.setBid(bid);
 
-        return getList(search);
+        return getList(search, status);
+    }
+
+    public ListData<BoardData> getList(String bid, BoardDataSearch search) {
+        return getList(bid, search, DeleteStatus.UNDELETED);
     }
 
     /**
      * 게시판 개별 조회
-     *
      * @param seq
      * @return
      */
-    public BoardData get(Long seq) {
+    public BoardData get(Long seq, DeleteStatus status) {
 
-        BoardData item = repository.findById(seq).orElseThrow(BoardDataNotFoundException::new);
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        QBoardData boardData = QBoardData.boardData;
+        andBuilder.and(boardData.seq.eq(seq));
 
-        //추가 데이터 처리
+        //삭제, 미삭제 게시글 조회 처리
+        if (status != DeleteStatus.ALL) {
+            if (status == DeleteStatus.UNDELETED) {
+                andBuilder.and(boardData.deletedAt.isNull()); //미삭제된 게시글
+            } else {
+                andBuilder.and(boardData.deletedAt.isNotNull()); //삭제된 게시글
+            }
+        }
+
+        BoardData item = queryFactory.selectFrom(boardData)
+                .leftJoin(boardData.board)
+                .fetchJoin()
+                .leftJoin(boardData.member)
+                .fetchJoin()
+                .where(andBuilder)
+                .fetchFirst();
+
+        if (item == null) {
+            throw new BoardDataNotFoundException();
+        }
+
+        // 추가 데이터 처리
         addInfo(item);
 
         return item;
     }
 
+    public BoardData get(Long seq) {
+        return get(seq, DeleteStatus.UNDELETED);
+    }
+
+    /**
+     * BoardData 엔티티 -> RequestBoard 커맨드 객체로 변환
+     *
+     * @param seq
+     * @return
+     */
+    public RequestBoard getForm(Long seq, DeleteStatus status) {
+        BoardData item = get(seq, status);
+
+        return getForm(item, status);
+    }
+
+    public RequestBoard getForm(BoardData item, DeleteStatus status) {
+        return new ModelMapper().map(item, RequestBoard.class);
+    }
+
+    public RequestBoard getForm(Long seq) {
+        return getForm(seq, DeleteStatus.UNDELETED);
+    }
+
+    public RequestBoard getForm(BoardData item) {
+        return getForm(item, DeleteStatus.UNDELETED);
+    }
     /**
      *  추가 데이터 처리
      *      - 업로드한 파일 목록
      *          에디터 이미지 목록, 첨부 파일 이미지 목록
      *      - 권한 : 글쓰기, 글수정, 글 삭제, 글 조회 가능 여부
-     *      - 댓글...
+     *      - 댓글 ..
      *
      * @param item
      */
-
     public void addInfo(BoardData item) {
 
     }
