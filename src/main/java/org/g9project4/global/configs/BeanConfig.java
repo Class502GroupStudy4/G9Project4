a3f350795dcbbe3ceca34884ecdae03b12b3adbc
yoojung275 <yoojung275@gmail.com>
2024-08-16 09:23:15 +0900

    게시판 구현 중

--- src/main/java/org/g9project4/global/configs/BeanConfig.java
+++ src/main/java/org/g9project4/global/configs/BeanConfig.java
@@ -2,6 +2,7 @@ package org.g9project4.global.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,13 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @RequiredArgsConstructor
 public class BeanConfig {
+
     private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
