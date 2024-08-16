bbfc999a8b070cfa83697a3e30fa1f2422b17bf5
yoojung275 <yoojung275@gmail.com>
2024-08-16 10:32:09 +0900

    게시판 구현 중

--- src/main/java/org/g9project4/board/services/BoardInfoService.java
+++ src/main/java/org/g9project4/board/services/BoardInfoService.java
@@ -1,4 +1,146 @@
 package org.g9project4.board.services;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.g9project4.board.controllers.BoardDataSearch;
+import org.g9project4.board.entities.BoardData;
+import org.g9project4.board.entities.QBoardData;
+import org.g9project4.board.repositories.BoardDataRepository;
+import org.g9project4.global.ListData;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class BoardInfoService {
+
+    private final JPAQueryFactory queryFactory;
+    private final BoardDataRepository repository;
+
+    /**
+     * 게시글 목록 조회
+     *
+     * @return
+     */
+    public ListData<BoardData> getList(BoardDataSearch search) {
+
+        int page = Math.max(search.getPage(), 1);
+        int limit = search.getLimit();
+
+        String sopt = search.getSopt();
+        String skey = search.getSkey();
+
+        String bid = search.getBid();
+        List<String> bids = search.getBids(); // 게시판 여러개 조회
+
+        /* 검색 처리 S */
+        QBoardData boardData = QBoardData.boardData;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+
+        if (bid != null && StringUtils.hasText(bid.trim())) { // 게시판별 조회
+            bids = List.of(bid);
+        }
+
+        if (bids != null && !bids.isEmpty()) { // 게시판 여러개 조회
+            andBuilder.and(boardData.board.bid.in(bids));
+        }
+
+        /**
+         * 조건 검색 처리
+         *
+         * sopt - ALL : 통합검색(제목 + 내용 + 글작성자(작성자, 회원명))
+         *       SUBJECT : 제목검색
+         *       CONTENT : 내용검색
+         *       SUBJECT_CONTENT: 제목 + 내용 검색
+         *       NAME : 이름(작성자, 회원명)
+         */
+        sopt = sopt != null && StringUtils.hasText(sopt.trim()) ? sopt.trim() : "ALL";
+        if (skey != null && StringUtils.hasText(skey.trim())) {
+            skey = skey.trim();
+            BooleanExpression condition = null;
+
+            BooleanBuilder orBuilder = new BooleanBuilder();
+
+            /* 이름 검색 S */
+            BooleanBuilder nameCondition = new BooleanBuilder();
+            nameCondition.or(boardData.poster.contains(skey));
+            if (boardData.member != null) {
+                nameCondition.or(boardData.member.userName.contains(skey));
+            }
+            /* 이름 검색 E */
+
+            if (sopt.equals("ALL")) { // 통합 검색
+                orBuilder.or(boardData.subject.concat(boardData.content)
+                                .contains(skey))
+                        .or(nameCondition);
+
+            } else if (sopt.equals("SUBJECT")) { // 제목 검색
+                condition = boardData.subject.contains(skey);
+            } else if (sopt.equals("CONTENT")) { // 내용 검색
+                condition = boardData.content.contains(skey);
+            } else if (sopt.contains("SUBJECT_CONTENT")) { // 제목 + 내용 검색
+                condition = boardData.subject.concat(boardData.content)
+                        .contains(skey);
+            } else if (sopt.equals("NAME")) {
+                andBuilder.and(nameCondition);
+            }
+
+            if (condition != null) andBuilder.and(condition);
+            andBuilder.and(orBuilder);
+        }
+
+        /* 검색 처리 E */
+
+        /* 정렬 처리 S */
+        String sort = search.getSort();
+
+        PathBuilder<BoardData> pathBuilder = new PathBuilder<>(BoardData.class, "boardData");
+        OrderSpecifier orderSpecifier = null;
+        Order order = Order.DESC;
+        if (sort != null && StringUtils.hasText(sort.trim())) {
+            // 정렬항목_방향   예) viewCount_DESC -> 조회수가 많은 순으로 정렬
+            String[] _sort = sort.split("_");
+            if (_sort[1].toUpperCase().equals("ASC")) {
+                order = Order.ASC;
+            }
+
+            orderSpecifier = new OrderSpecifier(order, pathBuilder.get(_sort[0]));
+        }
+        /* 정렬 처리 E */
+
+        return null;
+    }
+
+    /**
+     * 게시판 별 목록
+     *
+     * @param bid
+     * @param search
+     * @return
+     */
+    public ListData<BoardData> getList(String bid, BoardDataSearch search) {
+        search.setBid(bid);
+
+        return getList(search);
+    }
+
+    /**
+     * 게시판 개별 조회
+     *
+     * @param seq
+     * @return
+     */
+    public BoardData get(Long seq) {
+
+        return null;
+    }
 }
