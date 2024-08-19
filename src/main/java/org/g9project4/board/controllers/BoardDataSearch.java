5b1d04f246bc5db4b1428d1d0bfa810f94fa57cd
inseokLee999 <syt06137@naver.com>
2024-08-19 14:13:30 +0900

    BoardDataSearch 수정

--- src/main/java/org/g9project4/board/controllers/BoardDataSearch.java
+++ src/main/java/org/g9project4/board/controllers/BoardDataSearch.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 @Data
 public class BoardDataSearch extends CommonSearch {
-    private String bid; //게시판 ID
-    private List<String> bids; //게시한 ID 여러개
+    private int limit;
 
-    private List<String> sort; //정렬 조건
+    private String bid; // 게시판 ID
+    private List<String> bids; // 게시한 ID 여러개
 
+    private String sort; // 정렬 조건
 }
