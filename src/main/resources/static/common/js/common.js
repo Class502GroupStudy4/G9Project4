9efab92bae8667780e092721af91cce01817f937
youngsan01 <intokys@gmail.com>
2024-08-19 16:28:05 +0900

    회원가입, 로그인 css 변경

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -1,12 +1,8 @@
-const commonLib  = {
+const commonLib = {
     /**
      * ajax 요청 공통 기능
      *
-     * @param url
-     * @param method
-     * @param data
-     * @param headers
-     * @param responseType : 응답 데이터 타입 (text- text로, 그 외에는 JSON)
+     * @param responseType : 응답 데이터 타입(text - text로, 그외는 json)
      */
     ajaxLoad(url, method = "GET", data, headers, responseType) {
         if (!url) {
@@ -15,17 +11,19 @@ const commonLib  = {
 
         const csrfToken = document.querySelector("meta[name='csrf_token']")?.content?.trim();
         const csrfHeader = document.querySelector("meta[name='csrf_header']")?.content?.trim();
-        let rootUrl = document.querySelector("meta[name='rootUrl']")?.content?.trim() ?? '';
-        rootUrl = rootUrl === '/' ? '' : rootUrl;
 
-        url = location.protocol + "//" + location.host + rootUrl + url;
+        if (!/^http[s]?/i.test(url)) {
+            let rootUrl = document.querySelector("meta[name='rootUrl']")?.content?.trim() ?? '';
+            rootUrl = rootUrl === '/' ? '' : rootUrl;
 
+            url = location.protocol + "//" + location.host + rootUrl + url;
+        }
         method = method.toUpperCase();
         if (method === 'GET') {
             data = null;
         }
 
-        if (!(data instanceof FormData) && typeof data !== 'string' && data instanceof Object) {
+        if (data && !(data instanceof FormData) && typeof data !== 'string' && data instanceof Object) {
             data = JSON.stringify(data);
         }
 
@@ -37,13 +35,13 @@ const commonLib  = {
         const options = {
             method
         };
+
         if (data) options.body = data;
         if (headers) options.headers = headers;
 
         return new Promise((resolve, reject) => {
-
             fetch(url, options)
-                .then(res => responseType === 'text' ? res.text() : res.json()) // res.join() -> JSON / res.text() -> 텍스트
+                .then(res => responseType === 'text' ? res.text() : res.json()) // res.json() - JSON / res.text() - 텍스트
                 .then(data => resolve(data))
                 .catch(err => reject(err));
         });
@@ -51,11 +49,46 @@ const commonLib  = {
     /**
      * 에디터 로드
      *
-     * @param id
      */
     editorLoad(id) {
         if(!ClassicEditor || !id?.trim()) return;
 
         return ClassicEditor.create(document.getElementById(id.trim()), {});
     }
+};
+
+/**
+ * 이메일 인증 메일 보내기
+ *
+ * @param email : 인증할 이메일
+ */
+commonLib.sendEmailVerify = function(email) {
+    const { ajaxLoad } = commonLib;
+
+    const url = `/email/verify?email=${email}`;
+
+    ajaxLoad(url, "GET", null, null, "json")
+        .then(data => {
+            if (typeof callbackEmailVerify == 'function') { // 이메일 승인 코드 메일 전송 완료 후 처리 콜백
+                callbackEmailVerify(data);
+            }
+        })
+        .catch(err => console.error(err));
+};
+
+/**
+ * 인증 메일 코드 검증 처리
+ *
+ */
+commonLib.sendEmailVerifyCheck = function(authNum) {
+    const { ajaxLoad } = commonLib;
+    const url = `/email/auth_check?authNum=${authNum}`;
+
+    ajaxLoad(url, "GET", null, null, "json")
+        .then(data => {
+            if (typeof callbackEmailVerifyCheck == 'function') { // 인증 메일 코드 검증 요청 완료 후 처리 콜백
+                callbackEmailVerifyCheck(data);
+            }
+        })
+        .catch(err => console.error(err));
 };
