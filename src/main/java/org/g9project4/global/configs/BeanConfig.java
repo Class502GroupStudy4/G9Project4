2c63d18c734abde81750b65ea2a5c37046f1be99
youngsan01 <intokys@gmail.com>
2024-08-21 12:20:48 +0900

    게시판

--- src/main/java/org/g9project4/global/configs/BeanConfig.java
+++ src/main/java/org/g9project4/global/configs/BeanConfig.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.g9project4.publicData.tour.constants.ContentType;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
