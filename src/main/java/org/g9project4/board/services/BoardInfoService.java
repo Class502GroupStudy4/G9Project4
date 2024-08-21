076ca796d82b2d9be3494712b889e6fdbe392d41
yoojung275 <yoojung275@gmail.com>
2024-08-21 11:43:12 +0900

    게시판 쓰기 기능 및 수정 기능 수정

--- src/main/java/org/g9project4/board/services/BoardInfoService.java
+++ src/main/java/org/g9project4/board/services/BoardInfoService.java
@@ -17,6 +17,8 @@ import org.g9project4.board.entities.QBoardData;
 import org.g9project4.board.exceptions.BoardDataNotFoundException;
 import org.g9project4.board.repositories.BoardDataRepository;
 import org.g9project4.board.repositories.BoardRepository;
+import org.g9project4.file.entities.FileInfo;
+import org.g9project4.file.services.FileInfoService;
 import org.g9project4.global.ListData;
 import org.g9project4.global.Pagination;
 import org.g9project4.global.Utils;
@@ -40,6 +42,7 @@ public class BoardInfoService {
     private final JPAQueryFactory queryFactory;
     private final BoardDataRepository repository;
     private final BoardConfigInfoService configInfoService;
+    private final FileInfoService fileInfoService;
     private final BoardRepository boardRepository;
     private final HttpServletRequest request;
     private final ModelMapper modelMapper;
@@ -294,5 +297,13 @@ public class BoardInfoService {
      */
     public void addInfo(BoardData item) {
 
+        //업로드한 파일 목록 S
+        String gid = item.getGid();
+        List<FileInfo> editorImages = fileInfoService.getList(gid, "editor");
+        List<FileInfo> attachFiles = fileInfoService.getList(gid, "attach");
+
+        item.setEditorImages(editorImages);
+        item.setAttachFiles(attachFiles);
+        //업로드한 파일 목록 E
     }
 }
