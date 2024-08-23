c9dec0ba0561a3461ebb40ccbc47420351c53230
yoojung275 <yoojung275@gmail.com>
2024-08-23 20:43:26 +0900

    게시글 찜하기 기능

--- src/main/java/org/g9project4/board/services/BoardInfoService.java
+++ src/main/java/org/g9project4/board/services/BoardInfoService.java
@@ -20,10 +20,13 @@ import org.g9project4.board.repositories.BoardDataRepository;
 import org.g9project4.board.repositories.BoardRepository;
 import org.g9project4.file.entities.FileInfo;
 import org.g9project4.file.services.FileInfoService;
+import org.g9project4.global.CommonSearch;
 import org.g9project4.global.ListData;
 import org.g9project4.global.Pagination;
 import org.g9project4.global.Utils;
 import org.g9project4.global.constants.DeleteStatus;
+import org.g9project4.wishlist.constants.WishType;
+import org.g9project4.wishlist.services.WishListService;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -48,6 +51,7 @@ public class BoardInfoService {
     private final BoardDataRepository repository;
     private final BoardConfigInfoService configInfoService;
     private final FileInfoService fileInfoService;
+    private final WishListService wishListService;
     private final BoardRepository boardRepository;
     private final HttpServletRequest request;
     private final ModelMapper modelMapper;
@@ -290,6 +294,46 @@ public class BoardInfoService {
     public RequestBoard getForm(Long seq) {
         return getForm(seq, DeleteStatus.UNDELETED);
     }
+
+    /**
+     * 내가 찜한 게시글 목록
+     *
+     * @param search
+     * @return
+     */
+    public ListData<BoardData> getWishList(CommonSearch search) {
+        int page = Math.max(search.getPage(), 1);
+        int limit = search.getLimit();
+        limit = limit < 1 ? 10 : limit;
+        int offest = (page -1) * limit;
+
+
+        List<Long> seqs = wishListService.getList(WishType.BOARD);
+        if (seqs == null || seqs.isEmpty()) {
+            return new ListData<>();
+        }
+
+        QBoardData boardData = QBoardData.boardData;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(boardData.seq.in(seqs));
+
+        List<BoardData> items = queryFactory.selectFrom(boardData)
+                .where(andBuilder)
+                .leftJoin(boardData.board)
+                .fetchJoin()
+                .offset(offest)
+                .limit(limit)
+                .orderBy(boardData.createdAt.desc())
+                .fetch();
+
+        long total = repository.count(andBuilder);
+        int ranges = utils.isMobile() ? 5 : 10;
+        Pagination pagination = new Pagination(page, (int)total, ranges, limit, request);
+
+        return new ListData<>(items, pagination);
+
+    }
+
     /**
      *  추가 데이터 처리
      *      - 업로드한 파일 목록
--- src/main/java/org/g9project4/member/MemberUtil.java
+++ src/main/java/org/g9project4/member/MemberUtil.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.g9project4.member.constants.Authority;
 import org.g9project4.member.entities.Authorities;
 import org.g9project4.member.entities.Member;
+import org.g9project4.member.repositories.MemberRepository;
 import org.g9project4.member.services.MemberInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -18,8 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberUtil {
 
-    private final HttpSession session;
-    private MemberInfoService infoService;
+    private final MemberRepository repository;
 
     public boolean isLogin() {
         return getMember() != null;
@@ -39,7 +39,9 @@ public class MemberUtil {
 
         if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof MemberInfo memberInfo) {
 
-            return memberInfo.getMember();
+           Member member = repository.findByEmail(memberInfo.getEmail()).orElse(null);
+           memberInfo.setMember(member);
+           return member;
         }
 
             return null;
