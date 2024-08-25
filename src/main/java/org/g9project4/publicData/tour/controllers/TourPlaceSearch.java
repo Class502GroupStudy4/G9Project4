cf156fd5f3afb0097a57f1b8e93e9899dc490e30
inseokLee999 <syt06137@naver.com>
2024-08-26 08:57:24 +0900

    category 데이터 받아오기 완료

--- src/main/java/org/g9project4/publicData/tour/controllers/TourPlaceSearch.java
+++ src/main/java/org/g9project4/publicData/tour/controllers/TourPlaceSearch.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.g9project4.global.CommonSearch;
 import org.g9project4.publicData.tour.constants.ContentType;
-import org.g9project4.publicData.tour.entities.AreaCode;
-import org.g9project4.publicData.tour.entities.SigunguCode;
 
 import java.util.List;
 
@@ -20,15 +18,14 @@ public class TourPlaceSearch extends CommonSearch {
      * 필터 옵션
      * 1. 태그들
      * 1-1 AreaCode
-     *     1.구 소분류(시군구 코드)
+     * 1.구 소분류(시군구 코드)
      * 1-2 ContentType
      * 1-3 Category
-     *      1. category1
-     *      2. category2
-     *      3. category3
+     * 1. category1
+     * 2. category2
+     * 3. category3
      * 2. 검색 키워드
      * 3. 정렬 이름순, 최신순, 거리순, 인기순
-     *
      */
 
     private String areaCode;
@@ -37,6 +34,7 @@ public class TourPlaceSearch extends CommonSearch {
     private String category1;
     private String category2;
     private String category3;
+
     private Double latitude;
     private Double longitude;
     private Integer radius = 1000;
