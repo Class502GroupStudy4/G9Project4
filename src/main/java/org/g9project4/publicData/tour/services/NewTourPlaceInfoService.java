f7c4f29f8f323813b1fd8ed538a58abb44ab903a
jangjangji <sjjang41@naver.com>
2024-08-27 10:48:15 +0900

    커

--- src/main/java/org/g9project4/publicData/tour/services/NewTourPlaceInfoService.java
+++ src/main/java/org/g9project4/publicData/tour/services/NewTourPlaceInfoService.java
@@ -37,7 +37,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NewTourPlaceInfoService {
     private final RestTemplate restTemplate;
-    private final List contentType;
+//    private final List contentType;
     @PersistenceContext
     private EntityManager em;
 
