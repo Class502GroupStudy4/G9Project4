72cf6248902f6b729677931c101515d9ebf6bac3
minsukim002 <elancla14@gmail.com>
2024-08-28 19:16:24 +0900

    메인페이지 수정

--- src/main/java/org/g9project4/board/controllers/BoardController.java
+++ src/main/java/org/g9project4/board/controllers/BoardController.java
@@ -140,6 +140,7 @@ public class BoardController implements ExceptionProcessor {
         return utils.tpl("board/list");
     }
 
+
     @GetMapping("/view/{seq}")
     public String view(@PathVariable("seq") Long seq, @ModelAttribute BoardDataSearch search, Model model) {
         commonProcess(seq, "view", model);
