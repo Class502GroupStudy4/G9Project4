a08234391cace0d5e64f281d747cf49298f3fd9d
yoojung275 <yoojung275@gmail.com>
2024-08-28 16:49:51 +0900

    커밋

--- src/main/generated/org/g9project4/member/entities/QMember.java
+++ src/main/generated/org/g9project4/member/entities/QMember.java
@@ -7,7 +7,6 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
-import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -22,10 +21,6 @@ public class QMember extends EntityPathBase<Member> {
 
     public final org.g9project4.global.entities.QBaseEntity _super = new org.g9project4.global.entities.QBaseEntity(this);
 
-    public final DatePath<java.time.LocalDate> birth = createDate("birth", java.time.LocalDate.class);
-
-    public final ListPath<Authorities, QAuthorities> authorities = this.<Authorities, QAuthorities>createList("authorities", Authorities.class, QAuthorities.class, PathInits.DIRECT2);
-
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
 
@@ -35,10 +30,6 @@ public class QMember extends EntityPathBase<Member> {
     public final StringPath email = createString("email");
 
     public final StringPath gid = createString("gid");
-
-    public final ListPath<Interests, QInterests> interests = this.<Interests, QInterests>createList("interests", Interests.class, QInterests.class, PathInits.DIRECT2);
-
-    public final BooleanPath isForeigner = createBoolean("isForeigner");
 
     public final StringPath mobile = createString("mobile");
 
